c8aa4484af616053d7be313a26c8898afecc2f07
Steffy73 <117828767+Steffy73@users.noreply.github.com>
2022-11-09 09:53:20 -0300

    Add files via upload
    iniciando proyecto

---
+++ proyecto.py
@@ -0,0 +1,31 @@
+
+from random import *
+print("bienvenido a tu generador de contraseña")
+
+number = input("ingrese cantidad de contraseña: ")
+number = int("number") 
+
+
+
+legth = input("ingrese longitud de contraseña: ")
+legth = int(legth)
+
+
+
+letra=choice(["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","W","X","Y","Z"])
+letras=choice(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"])
+number1=choice(["1","2","3","4","5","6","7","8","9","#","$"])
+
+#print(letra + letras + number1)
+
+	
+print(password)
+ 
+
+
+
+    
+
+
+
+	
